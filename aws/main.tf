31d41eb4b4afe6fa858799d9e225a76085f0d262
Felix Roberge <roberge.felix@gmail.com>
2024-05-07 07:12:16 -0400

    add some element in AWS such as the gateway

--- aws/main.tf
+++ aws/main.tf
@@ -8,9 +8,9 @@ resource "aws_vpc" "main" {
   }
 }
 
-# resource "aws_internet_gateway" "gateway" {
-#   vpc_id = aws_vpc.main.id  
-# }
+resource "aws_internet_gateway" "gateway" {
+  vpc_id = aws_vpc.main.id  
+}
 
 resource "aws_subnet" "main" {
   vpc_id = aws_vpc.main.id
@@ -18,27 +18,18 @@ resource "aws_subnet" "main" {
   availability_zone = "${data.aws_region.current.name}a"
 }
 
-# resource "aws_route_table" "route_table" {
-#  vpc_id = aws_vpc.main.id
-#  route {
-#    cidr_block = "0.0.0.0/0"
-#    gateway_id = aws_internet_gateway.gateway.id
-#  }
-# }
+resource "aws_route_table" "route_table" {
+ vpc_id = aws_vpc.main.id
+ route {
+   cidr_block = "0.0.0.0/0"
+   gateway_id = aws_internet_gateway.gateway.id
+ }
+}
 
-# resource "aws_route_table_association" "route_table_association" {
-#  subnet_id      = aws_subnet.main.id
-#  route_table_id = aws_route_table.route_table.id
-# }
-
-# resource "tls_private_key" "key" {
-#   algorithm = "RSA"
-# }
-
-# resource "aws_key_pair" "aws_key" {
-#   key_name = "ansible-ssh-key"
-#   public_key = tls_private_key.key.public_key_openssh
-# }
+resource "aws_route_table_association" "route_table_association" {
+ subnet_id      = aws_subnet.main.id
+ route_table_id = aws_route_table.route_table.id
+}
 
 resource "aws_security_group" "allow_ssh" {
   name = "allow_ssh"
