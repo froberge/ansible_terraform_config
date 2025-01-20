ae48ba7dd3c9a152879589e07b47861020e71159
Felix Roberge <roberge.felix@gmail.com>
2025-01-20 14:53:57 -0500

    remove default value and refactor file

--- aws/variables.tf
+++ aws/variables.tf
@@ -10,6 +10,22 @@ variable "secret_key" {
     type = string
 }
 
+variable "instance_name_lb" {
+  type = string
+}
+
+variable "instance_name_webserver" {
+  type = string
+}
+
+variable "project_name" {
+  type = string
+}
+
+variable "instance_env" {
+  type = string
+}
+
 variable "network_interface_id" {
   type = string
   default = "network_id_from_aws"
@@ -23,24 +39,4 @@ variable "ami" {
 variable "key_instance_name" {
   type = string
   default = "ansible_terraform_demo"
-}
-
-variable "instance_name_lb" {
-  type = string
-  # default = "demo_lb_server"
-}
-
-variable "instance_name_webserver" {
-  type = string
-  # default = "demo_web_server"
-}
-
-variable "project_name" {
-  type = string
-  # default = "Ansible_Terraform_Demo"
-}
-
-variable "instance_env" {
-  type = string
-  # default = "dev"
 }
